9232a917396ef01d3f04d732cc8edf9cfb08693b
Hayden Parker <hayden@hkparker.com>
2021-07-13 21:43:52 -0400

    new thread object

--- ui/fyne.go
+++ ui/fyne.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"sort"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -11,12 +12,34 @@ import (
 )
 
 type Fyne struct {
-	app                fyne.App
-	threadVBox         *fyne.Container
-	entryBar           *container.Split
-	chatContainer      *fyne.Container
-	chatHistory        *container.Scroll
-	chatHistoriesViews map[string]*container.Scroll
+	app           fyne.App
+	threadVBox    *fyne.Container
+	entryBar      *container.Split
+	chatContainer *fyne.Container
+	chatHistory   *container.Scroll
+	threads       map[string]*thread
+	activeThread  string
+}
+
+type thread struct {
+	id          string
+	button      *widget.Button
+	scroll      *container.Scroll
+	lastMessage int64
+}
+
+// For sorting by last message received
+type threads []*thread
+
+func (t threads) Len() int {
+	return len(t)
+}
+func (t threads) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+func (t threads) Less(i, j int) bool {
+	// Reverse order, highest timestamp on top
+	return t[i].lastMessage > t[j].lastMessage
 }
 
 func (fyneUI *Fyne) simulate() {
@@ -29,31 +52,25 @@ func (fyneUI *Fyne) simulate() {
 	go func() {
 		for i := 0; i < 25; i++ {
 			fyneUI.ReceivedMessage("person1", "Person 1", "hello this is from person 1")
-			time.Sleep(500 * time.Millisecond)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			fyneUI.ReceivedMessage("person2", "Person 2", "hello this is from person 2")
+			time.Sleep(5 * time.Second)
 		}
 	}()
 
-	fyneUI.ReceivedMessage("person2", "Person 2", "hello this is from person 2")
-	fyneUI.ReceivedMessage("person1", "Person 1", "hello this is from person 1")
-	fyneUI.ReceivedMessage("person2", "Person 2", "hello this is from person 2")
 	fyneUI.ReceivedMessage("person3", "Person 3", "hello this is from person 3")
-	fyneUI.ReceivedMessage("person1", "Person 1", "hello this is from person 1")
-	/*
-		chat1 := widget.NewButton("Chat 1", func() {})
-		chat1.Importance = widget.LowImportance
-		chat2 := widget.NewButton("Chat 2", func() {})
-		chat2.Importance = widget.LowImportance
-		go func() {
-			time.Sleep(3 * time.Second)
-			chat2.Importance = widget.HighImportance
-			threads.Objects = []fyne.CanvasObject{threads.Objects[1], threads.Objects[0]}
-			threads.Refresh()
-		}()
-	*/
 }
 
+//
+// Lifecycle
+//
+
 func (fyneUI *Fyne) Build(configDirectory string) {
-	fyneUI.chatHistoriesViews = make(map[string]*container.Scroll)
+	fyneUI.threads = make(map[string]*thread)
 	fyneApp := app.New()
 	fyneUI.app = fyneApp
 	mainWindow := fyneUI.app.NewWindow("Bounce")
@@ -63,12 +80,13 @@ func (fyneUI *Fyne) Build(configDirectory string) {
 		log.WithFields(log.Fields{
 			"at": "desktop.DesktopUI.Build",
 		}).Info("window close button hit, shutting down")
-		//w.Close()
-		//a.Quit()
+		// There's some bug in Fyne where the app will hang when the close button
+		// is hit unless this is explicitly set
 		fyneUI.Quit()
 	})
 
-	threads := fyneUI.threadView()
+	fyneUI.threadVBox = container.NewVBox()
+	threads := container.NewVScroll(fyneUI.threadVBox)
 	fyneUI.chatContainer = container.NewMax(widget.NewLabel("Select a chat thread on the left"))
 	chatBox := container.NewVSplit(
 		fyneUI.chatContainer,
@@ -82,6 +100,19 @@ func (fyneUI *Fyne) Build(configDirectory string) {
 
 }
 
+func (fyneUI *Fyne) Run() {
+	go fyneUI.simulate()
+	fyneUI.app.Run()
+}
+
+func (fyneUI *Fyne) Quit() {
+	fyneUI.app.Quit()
+}
+
+//
+// User interface objects
+//
+
 func (fyneUI *Fyne) mainMenu() *fyne.MainMenu {
 	return fyne.NewMainMenu(
 		fyne.NewMenu(
@@ -93,61 +124,76 @@ func (fyneUI *Fyne) mainMenu() *fyne.MainMenu {
 	)
 }
 
-func (fyneUI *Fyne) threadView() *container.Scroll {
-	fyneUI.threadVBox = container.NewVBox()
-	return container.NewVScroll(fyneUI.threadVBox)
-}
-
 func (fyneUI *Fyne) textEntry() *container.Split {
-	fyneUI.entryBar = container.NewHSplit(
+	fyneUI.entryBar = container.NewHSplit( // TODO: something better for always smallest button
 		widget.NewMultiLineEntry(),
 		widget.NewButton("Send", func() {}),
 	)
 	return fyneUI.entryBar
 }
 
-func (fyneUI *Fyne) findOrCreateChatHistoryScroll(id string) *container.Scroll {
-	// if it exists in the UI's map, return it
-	if history, exists := fyneUI.chatHistoriesViews[id]; exists {
-		return history
+//
+// User interface manipulation
+//
+
+func (fyneUI *Fyne) displayHistory(thread *thread) {
+	fyneUI.activeThread = thread.id
+	fyneUI.chatContainer.Objects = []fyne.CanvasObject{thread.scroll}
+	fyneUI.chatContainer.Refresh()
+	thread.button.Importance = widget.LowImportance
+	thread.button.Refresh()
+}
+
+func (fyneUI *Fyne) isActive(thread *thread) bool {
+	return fyneUI.activeThread == thread.id
+}
+
+func (fyneUI *Fyne) refreshThreadOrder() {
+	allThreads := threads{}
+	for _, thread := range fyneUI.threads {
+		allThreads = append(allThreads, thread)
 	}
-	chatHistory := container.NewVBox()
-	chatHistoryScroll := container.NewVScroll(chatHistory)
-	fyneUI.chatHistoriesViews[id] = chatHistoryScroll
-	return chatHistoryScroll
-}
+	sort.Sort(allThreads)
 
-func (fyneUI *Fyne) Run() {
-	go fyneUI.simulate()
-	fyneUI.app.Run()
-}
-
-func (fyneUI *Fyne) Quit() {
-	fyneUI.app.Quit()
-}
-
-func (fyneUI *Fyne) AddThread(name string) { // TODO: add UUID
-	fyneUI.findOrCreateChatHistoryScroll(name)
-	newThread := widget.NewButton(name, func() {
-		chatHistory := fyneUI.findOrCreateChatHistoryScroll(name) // actually ID
-		fyneUI.chatContainer.Objects = []fyne.CanvasObject{chatHistory}
-		fyneUI.chatContainer.Refresh()
-		// TODO: how do I reference self in this function?
-		//newThread.Importance = widget.LowImportance
-		//newThread.Refresh()
-	})
-	newThread.Importance = widget.LowImportance
-	newThread.Alignment = widget.ButtonAlignLeading
-	fyneUI.threadVBox.Objects = append(
-		fyneUI.threadVBox.Objects,
-		newThread,
-	)
+	buttons := []fyne.CanvasObject{}
+	for _, thread := range allThreads {
+		buttons = append(buttons, thread.button)
+	}
+	fyneUI.threadVBox.Objects = buttons
 	fyneUI.threadVBox.Refresh()
 }
 
-func (fyneUI *Fyne) ReceivedMessage(threadID, username, message string) {
-	if thread, exists := fyneUI.chatHistoriesViews[threadID]; exists {
-		chatHistory := thread.Content.(*fyne.Container)
+//
+// Exported functions for the chat engine
+//
+
+func (fyneUI *Fyne) AddThread(id string) { // TODO: add group/user name add that as button
+	if _, exists := fyneUI.threads[id]; exists {
+		log.WithFields(log.Fields{
+			"id": id,
+		}).Warn("attempt to create a thread that already exists, ignored")
+		return
+	}
+
+	thread := &thread{
+		id:          id,
+		scroll:      container.NewVScroll(container.NewVBox()),
+		lastMessage: time.Now().Unix(),
+	}
+
+	thread.button = widget.NewButton(id, func() {
+		fyneUI.displayHistory(thread)
+	})
+	thread.button.Importance = widget.LowImportance
+	thread.button.Alignment = widget.ButtonAlignLeading
+
+	fyneUI.threads[id] = thread
+	fyneUI.refreshThreadOrder()
+}
+
+func (fyneUI *Fyne) ReceivedMessage(threadID, username, message string) { // TODO: this should probably take the protobuf definition
+	if thread, exists := fyneUI.threads[threadID]; exists {
+		chatHistory := thread.scroll.Content.(*fyne.Container)
 		usernameText := widget.NewLabel(username)
 		usernameText.TextStyle = fyne.TextStyle{Bold: true}
 		messageBox := container.NewVBox(
@@ -157,16 +203,23 @@ func (fyneUI *Fyne) ReceivedMessage(threadID, username, message string) {
 		)
 		chatHistory.Objects = append(chatHistory.Objects, messageBox)
 		chatHistory.Refresh()
-		// TODO: not working, always off by one.  Also only want this when open
-		// probably best to make a custom "thread" object using fyne objects internally
-		// to exist concepts like "IsOpen()" and "IsUnread()"
-		thread.ScrollToBottom()
-		// TODO: mark as important and sort to the top.  insertion sort by most
-		// recent unread
-		// TODO: need a refernce to the thread's button
-		//thread.Importance = widget.HighImportance
-		//thread.Refresh()
+
+		if fyneUI.isActive(thread) {
+			// TODO: not working, always off by one.  probably need something manual based on this
+			// https://github.com/fyne-io/fyne/blob/a25b08480d742f016ee24c1b37aa02c3bb433bd5/internal/widget/scroller.go#L473
+			// also: this should only happen when the user was already scrolled all the way down,
+			thread.scroll.ScrollToBottom()
+		} else {
+			// This thread isn't active, mark the button as unread
+			thread.button.Importance = widget.HighImportance
+			thread.button.Refresh()
+		}
+
+		thread.lastMessage = time.Now().Unix()
+		fyneUI.refreshThreadOrder()
 	} else {
-		log.Warn("thread does not exist: " + threadID)
+		log.WithFields(log.Fields{
+			"id": threadID,
+		}).Warn("thread does not exist on message receive")
 	}
 }
