394687299f8a3c7985897fc08b5c2dbf34b03bb4
Hayden Parker <hayden@hkparker.com>
2021-07-28 00:59:31 -0400

    some protocol ideas, pass peer address to network handlers

--- chat/network_handlers.go
+++ chat/network_handlers.go
@@ -6,14 +6,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (bounce *Bounce) getHandlers() map[uint16]func([]byte) { // TODO: custom types for these uint16s
-	return map[uint16]func([]byte){
+func (bounce *Bounce) getHandlers() map[uint16]func(BounceAddress, []byte) { // TODO: custom types for these uint16s
+	return map[uint16]func(BounceAddress, []byte){
 		0: bounce.handleChatMessage,
 	}
 }
 
 func (bounce *Bounce) handleIncomingConnection(conn net.Conn) {
 	handlers := bounce.getHandlers()
+	peer := BounceAddress(conn.RemoteAddr().String())
 	// Get the peer address
 	// reject it if it isn't a known device?  Maybe don't want to if introductions / group membership is out of order
 	// if it is known, add this connection to the larger device structure, which will mark the owner's user as online
@@ -26,19 +27,19 @@ func (bounce *Bounce) handleIncomingConnection(conn net.Conn) {
 		handler, ok := handlers[frameType]
 		if !ok {
 			log.WithFields(log.Fields{
-				"peer": conn.RemoteAddr(),
+				"peer": peer,
 				"type": frameType,
 			}).Error("peer sent an unsupported frame type, disconnecting")
 			// tell the larger device pool this connection is dead
 			// TODO: conn.Close()?
 			return
 		} else {
-			handler(data)
+			handler(peer, data)
 		}
 	}
 }
 
-func (bounce *Bounce) handleChatMessage(payload []byte) {
+func (bounce *Bounce) handleChatMessage(peer BounceAddress, payload []byte) {
 	// protobuf unmarshal the bytes
 	// put it in the database
 	// send it to the UI
