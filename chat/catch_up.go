22e09f2ddc3b04f3195e153f1fa965eaf666c30f
Hayden Parker <hayden@hkparker.com>
2024-04-18 20:06:38 -0500

    include NOP groups in same ack and load into catchup

--- chat/catch_up.go
+++ chat/catch_up.go
@@ -115,7 +115,7 @@ func (b *bounce) handleCatchUp(peer string, payload []byte, _ bool) broadcastabl
 
 		// Ignore frames for groups that we will not be a part of after this catch up
 		if b.isNopGroupFrame(nopGroups, fr) {
-			go b.sendAck(peer, fr.Type, fr.ID)
+			a.References = append(a.References, frameReference{FrameID: fr.ID, Type: fr.Type})
 			continue
 		}
 
