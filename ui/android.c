2c781e7d38af43d0b41978bd74c15740dd735bdc
Hayden Parker <hayden@hkparker.com>
2026-05-12 06:28:46 -0600

    use tabs everywhere

--- ui/android.c
+++ ui/android.c
@@ -8,25 +8,25 @@
 const char *batteryOptimizationsIgnored(uintptr_t java_vm, uintptr_t jni_env, uintptr_t ctx) {
 	JNIEnv *env = (JNIEnv*)jni_env;
 
-        jclass classNativeActivity = (*env)->FindClass(env, "android/app/NativeActivity");
+	jclass classNativeActivity = (*env)->FindClass(env, "android/app/NativeActivity");
 	if (classNativeActivity == NULL) {
 		return "android/app/NativeActivity not found";
 	}
-        jclass classPowerManager = (*env)->FindClass(env, "android/os/PowerManager");
+	jclass classPowerManager = (*env)->FindClass(env, "android/os/PowerManager");
 	if (classPowerManager == NULL) {
 		return "android/os/PowerManager not found";
 	}
 
-        jmethodID idNativeActivity_getSystemService = (*env)->GetMethodID(env, classNativeActivity, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
-        jstring stringPowerService = (*env)->NewStringUTF(env, "power");
-        jobject powerManager = (*env)->CallObjectMethod(env, (jobject)ctx, idNativeActivity_getSystemService, stringPowerService);
+	jmethodID idNativeActivity_getSystemService = (*env)->GetMethodID(env, classNativeActivity, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
+	jstring stringPowerService = (*env)->NewStringUTF(env, "power");
+	jobject powerManager = (*env)->CallObjectMethod(env, (jobject)ctx, idNativeActivity_getSystemService, stringPowerService);
 
 	jmethodID idPowerManager_isIgnoringBatteryOptimizations = (*env)->GetMethodID(env, classPowerManager, "isIgnoringBatteryOptimizations", "(Ljava/lang/String;)Z");
-        jstring stringBouncePackage = (*env)->NewStringUTF(env, "chat.bounce");
-        jboolean isIgnoredBool = (*env)->CallBooleanMethod(env, powerManager, idPowerManager_isIgnoringBatteryOptimizations, stringBouncePackage);
+	jstring stringBouncePackage = (*env)->NewStringUTF(env, "chat.bounce");
+	jboolean isIgnoredBool = (*env)->CallBooleanMethod(env, powerManager, idPowerManager_isIgnoringBatteryOptimizations, stringBouncePackage);
 
-        (*env)->DeleteLocalRef(env, stringBouncePackage);
-        (*env)->DeleteLocalRef(env, stringPowerService);
+	(*env)->DeleteLocalRef(env, stringBouncePackage);
+	(*env)->DeleteLocalRef(env, stringPowerService);
 
 	if (isIgnoredBool == JNI_TRUE) {
 		return "ignored";
@@ -37,19 +37,19 @@ const char *batteryOptimizationsIgnored(uintptr_t java_vm, uintptr_t jni_env, ui
 const char *requestIgnoreBatteryOptimizations(uintptr_t java_vm, uintptr_t jni_env, uintptr_t ctx) {
 	JNIEnv *env = (JNIEnv*)jni_env;
 
-        jclass classIntent = (*env)->FindClass(env, "android/content/Intent");
+	jclass classIntent = (*env)->FindClass(env, "android/content/Intent");
 	if (classIntent == NULL) {
 		return "android/content/Intent not found";
 	}
 
 	// Create an intent
-        jmethodID idIntent_contructor = (*env)->GetMethodID(env, classIntent, "<init>", "()V");
-        jobject intent = (*env)->NewObject(env, classIntent, idIntent_contructor);
+	jmethodID idIntent_contructor = (*env)->GetMethodID(env, classIntent, "<init>", "()V");
+	jobject intent = (*env)->NewObject(env, classIntent, idIntent_contructor);
 
 	// Set the action on the intent
-        jmethodID idIntent_setAction = (*env)->GetMethodID(env, classIntent, "setAction", "(Ljava/lang/String;)Landroid/content/Intent;");
+	jmethodID idIntent_setAction = (*env)->GetMethodID(env, classIntent, "setAction", "(Ljava/lang/String;)Landroid/content/Intent;");
 	jstring stringRequestIgnoreBatteryOptimizations = (*env)->NewStringUTF(env, "android.settings.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS");
-        jobject intentWithAction = (*env)->CallObjectMethod(env, intent, idIntent_setAction, stringRequestIgnoreBatteryOptimizations);
+	jobject intentWithAction = (*env)->CallObjectMethod(env, intent, idIntent_setAction, stringRequestIgnoreBatteryOptimizations);
 
 	// Parse the package name as a URI
 	jclass classUri = (*env)->FindClass(env, "android/net/Uri");
@@ -61,7 +61,7 @@ const char *requestIgnoreBatteryOptimizations(uintptr_t java_vm, uintptr_t jni_e
 	jobject parsedUriData = (*env)->CallStaticObjectMethod(env, classUri, idUri_parse, stringBouncePackage);
 
 	// Set the URI as the intent data
-        jmethodID idIntent_setData = (*env)->GetMethodID(env, classIntent, "setData", "(Landroid/net/Uri;)Landroid/content/Intent;");
+	jmethodID idIntent_setData = (*env)->GetMethodID(env, classIntent, "setData", "(Landroid/net/Uri;)Landroid/content/Intent;");
 	(*env)->CallObjectMethod(env, intent, idIntent_setData, parsedUriData);
 
 	// Start the intent
@@ -69,12 +69,12 @@ const char *requestIgnoreBatteryOptimizations(uintptr_t java_vm, uintptr_t jni_e
 	if (classUri == NULL) {
 		return "context class not found";
 	}
-        jmethodID idContext_startActivity = (*env)->GetMethodID(env, classContext, "startActivity", "(Landroid/content/Intent;)V");
-        (*env)->CallVoidMethod(env, (jobject)ctx, idContext_startActivity, intentWithAction);
+	jmethodID idContext_startActivity = (*env)->GetMethodID(env, classContext, "startActivity", "(Landroid/content/Intent;)V");
+	(*env)->CallVoidMethod(env, (jobject)ctx, idContext_startActivity, intentWithAction);
 
 	// Clean up references
-        (*env)->DeleteLocalRef(env, stringRequestIgnoreBatteryOptimizations);
-        (*env)->DeleteLocalRef(env, stringBouncePackage);
+	(*env)->DeleteLocalRef(env, stringRequestIgnoreBatteryOptimizations);
+	(*env)->DeleteLocalRef(env, stringBouncePackage);
 
 	return "";
 }
